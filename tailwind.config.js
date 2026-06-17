110aec05b173ae3506e69f934b3e679b22227398
Carlos Henrique Tourinho Santana <henriquetourinho@riseup.net>
2026-06-17 13:48:30 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'oil-black': '#161513',
+        'plate-yellow': '#FFC72C',
+        'shop-red': '#C8102E',
+        'shop-red-dark': '#8E0B20',
+        'steel': '#3A3A38',
+        'steel-light': '#5C5B57',
+        'cream': '#F2EDE4',
+      },
+      fontFamily: {
+        'display': ['Archivo Black', 'sans-serif'],
+        'oswald': ['Oswald', 'sans-serif'],
+        'body': ['Inter', 'sans-serif'],
+      }
+    }
+  }
+}
